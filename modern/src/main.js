0de7f921619ae8ab51a0271f5739a6a1a7e82c7b
Akdev Saha <akdevsaha2003@gmail.com>
2026-03-05 15:50:56 +0530

    initital 3d modelling

---
+++ modern/src/main.js
@@ -0,0 +1,63 @@
+import * as THREE from "three"
+import {OrbitControls} from "three/addons/controls/OrbitControls.js"
+const canvas = document.getElementById("canvas")
+
+// 1. scene
+
+const scene = new THREE.Scene()
+scene.background= new THREE.Color("#F0F0F0")
+
+//2. camera
+const camera = new THREE.PerspectiveCamera(55, window.innerWidth/ window.innerHeight, 0.1, 1000 )
+camera.position.z=5
+
+//3. onbject
+
+const geometry = new THREE.DodecahedronGeometry()
+const material = new THREE.MeshLambertMaterial({color: "#468585", emissive: "#468585"})
+const dodecahedron = new THREE.Mesh(geometry, material)
+
+const boxGeometry = new THREE.BoxGeometry(2,0.1,2)
+const boxMaterial = new THREE.MeshStandardMaterial({color: "#B4B4B3", emissive: "#B4B4B3"})
+const box = new THREE.Mesh(boxGeometry, boxMaterial)
+box.position.y= -1.5;
+scene.add(dodecahedron, box)
+
+//4. light
+
+const light = new THREE.SpotLight(0x006769, 100)
+light.position.set(2,1,0.5)
+scene.add(light)
+
+//5. renderer
+const renderer= new THREE.WebGLRenderer({canvas})
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.render(scene, camera)
+
+//6. add orbitControls
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true;
+controls.dampingFactor= 0.05;
+controls.enableZoom= true;
+controls.enablePan = true;
+
+//7. add animation
+
+function animate(){
+requestAnimationFrame(animate)  
+dodecahedron.rotation.x +=0.01
+dodecahedron.rotation.y +=0.01
+
+box.rotation.y +=0.005
+controls.update()
+renderer.render(scene, camera)
+}
+// 8. handle window resize
+window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+})
+animate()
